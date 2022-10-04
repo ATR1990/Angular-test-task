e95367c21346a11ac378cdcff95f350f109f379e
AltynbekovT <tauyekelaltynbekov@gmail.com>
2022-10-04 14:32:21 +0600

    small fix

--- src/sk/shared/services/cars.service.ts
+++ src/sk/shared/services/cars.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core'
 import {HttpClient} from "@angular/common/http"
 import {Observable, Subject} from "rxjs"
 
-import {CarInterface} from "../models/car.interface"
+import {CarInterface} from "@shared/models/car.interface"
 import {GET_CARS_LIST} from "@shared/constants/url"
 
 @Injectable({
